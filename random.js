1eeae00d0949049b610bc63c674dd0867f416544
JungIngook <wjddlsrnr307@gmail.com>
2020-03-27 17:38:24 +0900

    #과제 수정 새로운공식

--- random.js
+++ random.js
@@ -3,10 +3,11 @@
 
 10 이상 20 이하의 정수를 임의로 30개 출력하는 코드를 구현하시오.
 */
-for (let i=0;i<=10;i++) {
-    console.log(Math.floor(Math.random()* 20 +10))
+for (let i=0;i<=30;i++) {
+    // console.log(Math.floor(Math.random()* 10)+10) 
+    console.log(Math.floor(Math.random()* (20-10+1))+10)
 }
-/* 
+/*
     중요 : 랜덤값이 포함되어 있으므로 결과값은 상황에 따라 각기 다름
     콘솔 :
             11
